12373fca17bc025f593cdc34f6abd3eb9a08885f
strokegdz <154758467+strokegmd@users.noreply.github.com>
2025-09-12 10:16:01 +0300

    Update mtproto_dc_options.cpp

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -28,8 +28,8 @@ struct BuiltInDc {
 	int port;
 };
 
-#define IpV4 "192.168.1.100"
-#define IpV6 "192.168.1.100"
+#define IpV4 "voidgram.lol"
+#define IpV6 "voidgram.lol"
 #define IpV4Port 20443
 #define IpV4Port2 20443
 #define IpV6Port 20443
